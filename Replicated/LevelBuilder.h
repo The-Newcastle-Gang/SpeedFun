44ca0711e66b65717c51965cd28bb5de3c0f1628
AdSand <149825252+AdSand@users.noreply.github.com>
2024-02-01 11:17:50 +0000

    Added setup for the level builder class
    Also added comments on what needs done, in what order

---
+++ Replicated/LevelBuilder.h
@@ -0,0 +1,14 @@
+#pragma once
+
+using namespace std;
+using namespace NCL;
+using namespace CSC8503;
+using namespace std;
+
+class LevelBuilder
+{
+public:
+	void BuildLevel();
+protected:
+	bool levelBuilt = false;
+};
