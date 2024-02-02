0ac53b6ef72ec35b519dc7838c6de5f7b8b4fd17
JunkyX1122 <122457074+JunkyX1122@users.noreply.github.com>
2024-02-02 11:17:43 +0000

    Main uses client state machine

--- Client/main.cpp
+++ Client/main.cpp
@@ -13,17 +13,13 @@
 
 #include "TutorialGame.h"
 
+#include "ClientGameStateMachine.h"
 using namespace NCL;
 using namespace CSC8503;
 
 
-int main() {
-    Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1280, 720);
-
-    if (!w->HasInitialised()) {
-        return -1;
-    }
-
+void RunTutorialGame(Window* w)
+{
     w->ShowOSPointer(false);
     w->LockMouseToWindow(true);
 
@@ -51,4 +47,44 @@ int main() {
         g->UpdateGame(dt);
     }
     Window::DestroyGameWindow();
+}
+
+void RunClientGameStateMachine(Window* w)
+{
+    w->GetTimer()->GetTimeDeltaSeconds(); 
+    ClientGameStateMachine* client = new ClientGameStateMachine;
+
+    while (w->UpdateWindow() && !Window::GetKeyboard()->KeyDown(KeyboardKeys::ESCAPE)) {
+        float dt = w->GetTimer()->GetTimeDeltaSeconds();
+        if (dt > 0.1f) {
+            std::cout << "Skipping large time delta" << std::endl;
+            continue;
+        }
+        if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::PRIOR)) {
+            w->ShowConsole(true);
+        }
+        if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::NEXT)) {
+            w->ShowConsole(false);
+        }
+        if (Window::GetKeyboard()->KeyPressed(KeyboardKeys::T)) {
+            w->SetWindowPosition(0, 0);
+        }
+
+        w->SetTitle("Gametech frame time:" + std::to_string(1000.0f * dt));
+        client->Update(dt);
+    }
+    Window::DestroyGameWindow();
+}
+
+
+int main() {
+    Window*w = Window::CreateGameWindow("CSC8503 Game technology!", 1280, 720);
+
+    if (!w->HasInitialised()) {
+        return -1;
+    }
+
+    //RunTutorialGame(w);
+    RunClientGameStateMachine(w);
+
 }
