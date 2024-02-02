4fb93d4a75b7c6f167d1a55242a4b4fcf34e4f39
JunkyX1122 <122457074+JunkyX1122@users.noreply.github.com>
2024-02-02 10:57:24 +0000

    Readded GameStateInactive

---
+++ Client/States/GameStateInactive.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "State.h"
+#include <iostream>
+#include "./../Client/Renderer.h"
+#include "PhysicsSystem.h"
+#include "GameWorld.h"
+namespace NCL {
+	namespace CSC8503 {
+		class Inactive : public State
+		{
+		public:
+			Inactive(GameTechRenderer* rendererRef, GameWorld* gameWorldRef);
+			~Inactive();
+			void Update(float dt);
+
+			void OnEnter();
+			void OnExit();
+		protected:
+#ifdef USEVULKAN
+			GameTechVulkanRenderer* renderer;
+#else
+			GameTechRenderer* renderer;
+#endif
+			PhysicsSystem* physics;
+			GameWorld* world;
+		};
+	}
+}
