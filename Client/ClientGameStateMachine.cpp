010bfb0320e680a63ca8c1501514476f65157f85
JunkyX1122 <122457074+JunkyX1122@users.noreply.github.com>
2024-02-02 11:06:35 +0000

    Readded the ClientGameStateMachine

--- Client/ClientGameStateMachine.cpp
+++ Client/ClientGameStateMachine.cpp
@@ -1,45 +1,63 @@
-#pragma once
-#include "StateMachine.h"
-#include <vector>
-#include "Renderer.h"
-#include "GameWorld.h"
+#include "ClientGameStateMachine.h"
+#include "State.h"
+#include "StateTransition.h"
+#include "GameStateInactive.h"
+#include "GameStateLoading.h"
+#include "GameStateMenu.h"
+#include "GameStateInGameplay.h"
+using namespace NCL;
+using namespace CSC8503;
+ClientGameStateMachine::ClientGameStateMachine() : StateMachine()
+{
+	gameWorld = new GameWorld();
+	renderer = new GameTechRenderer(*gameWorld);
+	currentClientState = ClientStates::InactiveState;
+	InitialiseClientStateMachine();
+}
 
-namespace NCL {
-	namespace CSC8503 {
-		enum ClientStates {
-			InactiveState,
-			LoadingState,
-			MenuState,
-			InGameplayState
-		};
-		class ClientGameStateMachine : public StateMachine {
-		public:
-			ClientGameStateMachine();
-			virtual ~ClientGameStateMachine();
-			std::vector<State*> GetAvailableLevels()
-			{
-				return availableLevels;
-			}
-			void SetClientState(ClientStates clientState)
-			{
-				currentClientState = clientState;
-			}
-		protected:
+ClientGameStateMachine::~ClientGameStateMachine()
+{
+}
+
+void ClientGameStateMachine::InitialiseClientStateMachine()
+{
+	//*
+	Inactive* clientInactive = new Inactive(renderer, gameWorld);
+	Loading* clientLoading = new Loading(renderer, gameWorld);
+	Menu* clientMenu = new Menu(renderer, gameWorld);
+	InGameplay* clientInGameplay = new InGameplay(renderer, gameWorld);
+
+	StateTransition* inactiveToLoading = new StateTransition(clientInactive, clientLoading, [&](void)->bool {
+		return ForceTransition(Window::GetKeyboard()->KeyPressed(KeyboardKeys::SPACE), ClientStates::LoadingState);
+		});
+	StateTransition* loadingToMenu = new StateTransition(clientLoading, clientMenu, [&](void)->bool {
+		return ForceTransition(Window::GetKeyboard()->KeyPressed(KeyboardKeys::L), ClientStates::MenuState);
+		});
+	StateTransition* menuToLoading = new StateTransition(clientMenu, clientLoading, [&](void)->bool {
+		return ForceTransition(Window::GetKeyboard()->KeyPressed(KeyboardKeys::SPACE), ClientStates::LoadingState);
+		});
+	StateTransition* loadingToInGameplay = new StateTransition(clientLoading, clientInGameplay, [&](void)->bool {
+		return ForceTransition(Window::GetKeyboard()->KeyPressed(KeyboardKeys::SPACE), ClientStates::InGameplayState);
+		});
+	StateTransition* inGameplayToLoading = new StateTransition(clientInGameplay, clientLoading, [&](void)->bool {
+		int tester = clientInGameplay->ExitType();
+		return ForceTransition(tester != ExitStates::Invalid, ClientStates::LoadingState);
+		});
+
+	this->AddState(clientInactive);
+	this->AddState(clientLoading);
+	this->AddState(clientMenu);
+	this->AddState(clientInGameplay);
+
+	this->AddTransition(inactiveToLoading);
+	this->AddTransition(loadingToMenu);
+	this->AddTransition(menuToLoading);
+	this->AddTransition(loadingToInGameplay);
+	this->AddTransition(inGameplayToLoading);
+}
 
 
-			ClientStates currentClientState;
-			std::vector<State*> availableLevels;
-			void InitialiseClientStateMachine();
-
-			bool ForceTransition(bool hasConidtionMet, ClientStates stateSwitchTo);
-
-#ifdef USEVULKAN
-			GameTechVulkanRenderer* renderer;
-#else
-			GameTechRenderer* renderer;
-#endif
-
-			GameWorld* gameWorld;
-		};
-	}
+bool ClientGameStateMachine::ForceTransition(bool hasConidtionMet, ClientStates stateSwitchTo) {
+	if (hasConidtionMet) currentClientState = stateSwitchTo;
+	return hasConidtionMet;
 }
