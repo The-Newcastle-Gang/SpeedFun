b02dd721d32c2a79614995e732529832b41570c6
AdSand <149825252+AdSand@users.noreply.github.com>
2024-02-02 12:29:29 +0000

    Updated namespace stuff

--- Client/DebugMode.cpp
+++ Client/DebugMode.cpp
@@ -1,6 +1,6 @@
 #include "DebugMode.h"
 
-void DebugMode::DisplayDebug(float dt)
+void NCL::CSC8503::DebugMode::DisplayDebug(float dt)
 {
 	Debug::Print("FPS: " + std::to_string(GetFPSCount(dt)), Vector2(3, 50), Debug::MAGENTA);
 }
