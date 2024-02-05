c6058990c2830c56fa71b2b5ca3b6d9e9393b07d
ikozoom <80098430+ikozoom@users.noreply.github.com>
2024-02-05 17:20:12 +0000

    Update StageTimer.h

--- Replicated/StageTimer.h
+++ Replicated/StageTimer.h
@@ -1,1 +1,28 @@
-awdw
+#pragma once
+#include <chrono>
+namespace NCL {
+    class StageTimer {
+    public:
+        StageTimer(double goldTime, double silverTime, double bronzeTime);
+        void Start();
+        void Stop();
+        void Restart();
+        void SetPlayerTime(double time);
+        void ShowTimer();
+        void ReturnScore();
+
+    private:
+        typedef std::chrono::time_point<std::chrono::high_resolution_clock> Timepoint;
+        Timepoint startTime;
+        Timepoint endTime;
+        Timepoint restartTime;
+        Timepoint restartWaitingPeriod;
+        double goldTimePeriod;
+        double silverTimePeriod;
+        double bronzeTimePeriod;
+        double totalTimeSeconds;
+        double playerTime;      
+        bool isPlaying = false;
+        bool isRestart = false;
+    };
+}awdw
