f9da4abeaf371bc132829f471e9eb7d82254f6c9
Odycious10 <o.taylor360@hotmail.co.uk>
2024-02-06 16:50:52 +0000

    Update TutorialGame.h

--- Client/TutorialGame.h
+++ Client/TutorialGame.h
@@ -6,96 +6,107 @@
 
 
 namespace NCL {
-	namespace CSC8503 {
-		class TutorialGame		{
-		public:
-			TutorialGame();
-			~TutorialGame();
+    namespace CSC8503 {
+        class TutorialGame {
+        public:
+            TutorialGame();
 
-			virtual void UpdateGame(float dt);
+            ~TutorialGame();
 
-		protected:
-			void InitialiseAssets();
+            virtual void UpdateGame(float dt);
 
-			void InitCamera();
-			void UpdateKeys();
+        protected:
+            void InitialiseAssets();
 
-			void InitWorld();
+            void InitCamera();
 
-			/*
-			These are some of the world/object creation functions I created when testing the functionality
-			in the module. Feel free to mess around with them to see different objects being created in different
-			test scenarios (constraints, collision types, and so on). 
-			*/
-			void InitGameExamples();
+            void UpdateKeys();
 
-			void InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius);
-			void InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing);
-			void InitCubeGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, const Vector3& cubeDims);
+            void InitWorld();
 
-			void InitDefaultFloor();
+            /*
+            These are some of the world/object creation functions I created when testing the functionality
+            in the module. Feel free to mess around with them to see different objects being created in different
+            test scenarios (constraints, collision types, and so on).
+            */
+            void InitGameExamples();
 
-			bool SelectObject();
-			void MoveSelectedObject();
-			void DebugObjectMovement();
-			void LockedObjectMovement();
+            void InitSphereGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, float radius);
 
-			GameObject* AddFloorToWorld(const Vector3& position);
-			GameObject* AddSphereToWorld(const Vector3& position, float radius, float inverseMass = 10.0f);
-			GameObject* AddCubeToWorld(const Vector3& position, Vector3 dimensions, float inverseMass = 10.0f);
+            void InitMixedGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing);
 
-			GameObject* AddPlayerToWorld(const Vector3& position);
-			GameObject* AddEnemyToWorld(const Vector3& position);
-			GameObject* AddBonusToWorld(const Vector3& position);
+            void
+            InitCubeGridWorld(int numRows, int numCols, float rowSpacing, float colSpacing, const Vector3 &cubeDims);
+
+            void InitDefaultFloor();
+
+            bool SelectObject();
+
+            void MoveSelectedObject();
+
+            void DebugObjectMovement();
+
+            void LockedObjectMovement();
+
+            GameObject *AddFloorToWorld(const Vector3 &position);
+
+            GameObject *AddSphereToWorld(const Vector3 &position, float radius, float inverseMass = 10.0f);
+
+            GameObject *AddCubeToWorld(const Vector3 &position, Vector3 dimensions, float inverseMass = 10.0f);
+
+            GameObject *AddPlayerToWorld(const Vector3 &position);
+
+            GameObject *AddEnemyToWorld(const Vector3 &position);
+
+            GameObject *AddBonusToWorld(const Vector3 &position);
 
             void ClickTestFunc();
 
 #ifdef USEVULKAN
-			GameTechVulkanRenderer*	renderer;
+            GameTechVulkanRenderer*	renderer;
 #else
-			GameTechRenderer* renderer;
+            GameTechRenderer *renderer;
 #endif
-			PhysicsSystem*		physics;
-			GameWorld*			world;
+            PhysicsSystem *physics;
+            GameWorld *world;
 
-			bool useGravity;
-			bool inSelectionMode;
+            bool useGravity;
+            bool inSelectionMode;
 
-			float		forceMagnitude;
+            float forceMagnitude;
 
-			GameObject* selectionObject = nullptr;
+            GameObject *selectionObject = nullptr;
 
-			MeshGeometry*	capsuleMesh = nullptr;
-			MeshGeometry*	cubeMesh	= nullptr;
-			MeshGeometry*	sphereMesh	= nullptr;
+            MeshGeometry *capsuleMesh = nullptr;
+            MeshGeometry *cubeMesh = nullptr;
+            MeshGeometry *sphereMesh = nullptr;
 
-			TextureBase*	basicTex	= nullptr;
-			ShaderBase*		basicShader = nullptr;
+            TextureBase *basicTex = nullptr;
+            ShaderBase *basicShader = nullptr;
 
-			//Coursework Meshes
-			MeshGeometry*	charMesh	= nullptr;
-			MeshGeometry*	enemyMesh	= nullptr;
-			MeshGeometry*	bonusMesh	= nullptr;
+            //Coursework Meshes
+            MeshGeometry *charMesh = nullptr;
+            MeshGeometry *enemyMesh = nullptr;
+            MeshGeometry *bonusMesh = nullptr;
 
-			//Coursework Additional functionality	
-			GameObject* lockedObject	= nullptr;
-			Vector3 lockedOffset		= Vector3(0, 14, 20);
-			void LockCameraToObject(GameObject* o) {
-				lockedObject = o;
-			}
+            //Coursework Additional functionality
+            GameObject *lockedObject = nullptr;
+            Vector3 lockedOffset = Vector3(0, 14, 20);
 
-			GameObject* objClosest = nullptr;
+            void LockCameraToObject(GameObject *o) {
+                lockedObject = o;
+            }
+
+            GameObject *objClosest = nullptr;
+
+            // test stuff
+            LevelReader *levelReader;
+            LevelBuilder *levelBuilder;
+
+            void BuildLevelFromJSON(std::string levelName);
 
 
-       void LoadScripting();
-    };
-
-			// test stuff
-			LevelReader* levelReader;
-      LevelBuilder* levelBuilder;
-			void BuildLevelFromJSON(std::string levelName);
-		};
-
-	}
+            void LoadScripting();
+        };
+    }
 }
-
