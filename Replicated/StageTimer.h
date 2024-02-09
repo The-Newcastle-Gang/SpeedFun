cbb448da2802e16a2356a7608df7b0b99fb7a04a
ikozoom <80098430+ikozoom@users.noreply.github.com>
2024-02-09 15:45:00 +0000

    Update StageTimer.h

--- Replicated/StageTimer.h
+++ Replicated/StageTimer.h
@@ -1,1 +1,19 @@
-awdw
+#pragma once
+#include <chrono>
+namespace NCL {
+    class StageTimer {
+    public:
+        StageTimer();
+        void Start(float dt);
+        void Stop(float dt);
+        void Continue(float dt);
+        void ShowTimer(float dt);
+
+    private:
+        typedef std::chrono::time_point<std::chrono::high_resolution_clock> Timepoint;
+        double startTime;
+        double elapsedSeconds;
+        bool isPlaying = false;
+        bool isContinue = false;
+    };
+}
