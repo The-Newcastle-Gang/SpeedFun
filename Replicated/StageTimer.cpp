4a62f9cbe18fde742262a576507e993b1b8b05c6
ikozoom <80098430+ikozoom@users.noreply.github.com>
2024-02-09 15:43:54 +0000

    Update StageTimer.cpp

--- Replicated/StageTimer.cpp
+++ Replicated/StageTimer.cpp
@@ -1,1 +1,42 @@
-#include <StageTimer.h>
+#include "StageTimer.h"
+#include "Debug.h"
+using namespace NCL;
+StageTimer::StageTimer()
+    :isPlaying(false) {
+    startTime = 0.0;
+    elapsedSeconds = 0.0;
+}
+    void StageTimer::ShowTimer(float dt) {
+        if (isPlaying||isContinue) {
+            elapsedSeconds += dt;
+            Debug::Print("Time: " + std::to_string(elapsedSeconds) + " s", Vector2(5, 20), Debug::RED);
+        }
+        if (!isPlaying&&!isContinue) {
+            Debug::Print("Time: " + std::to_string(elapsedSeconds) + " s", Vector2(5, 20), Debug::RED);
+        }
+
+     }
+
+
+    void StageTimer::Start(float dt) {
+        if (!isPlaying&&!isContinue) {
+            startTime = dt;
+            elapsedSeconds = 0.0;
+            isPlaying = true;
+            isContinue = false;
+        }
+    }
+
+    void StageTimer::Stop(float dt) {
+        if (isPlaying||isContinue) {
+            isPlaying = false;
+            isContinue = false;
+        }
+    }
+    
+    void StageTimer::Continue(float dt) {
+        if (!isContinue && !isPlaying) {
+            isContinue = true;
+            isPlaying = false;
+        }
+    }
