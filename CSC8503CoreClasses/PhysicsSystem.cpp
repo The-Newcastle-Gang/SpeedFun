fa1cf75c1ae7844189284c312c4bbf2086f3d064
JunkyX1122 <122457074+JunkyX1122@users.noreply.github.com>
2024-03-05 11:52:20 +0000

    oops

--- CSC8503CoreClasses/PhysicsSystem.cpp
+++ CSC8503CoreClasses/PhysicsSystem.cpp
@@ -110,7 +110,7 @@ void PhysicsSystem::Update(float dt) {
 			BasicCollisionDetection();
 		}
         IntegrateAccel(realDT);
-		DrawAllObjectCollision();
+		if(isDebugDrawingCollision) DrawAllObjectCollision();
 		//This is our simple iterative solver - 
 		//we just run things multiple times, slowly moving things forward
 		//and then rechecking that the constraints have been met		
