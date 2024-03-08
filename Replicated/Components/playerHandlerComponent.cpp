7df7d47e2c5e64ef2477f07e78739b0b55276e14
Odycious10 <o.taylor360@hotmail.co.uk>
2024-03-08 17:01:39 +0000

    Created playerHandlerComponent and added it to Cmake

---
+++ Replicated/Components/playerHandlerComponent.cpp
@@ -0,0 +1,5 @@
+//
+// Created by c3064585 on 08/03/2024.
+//
+
+#include "playerHandlerComponent.h"
---
+++ Replicated/Components/playerHandlerComponent.h
@@ -0,0 +1,17 @@
+//
+// Created by c3064585 on 08/03/2024.
+//
+
+#ifndef SPEEDFUN_PLAYERHANDLERCOMPONENT_H
+#define SPEEDFUN_PLAYERHANDLERCOMPONENT_H
+#include "Component.h"
+
+using namespace NCL;
+using namespace CSC8503;
+
+class playerHandlerComponent : public Component {
+
+};
+
+
+#endif //SPEEDFUN_PLAYERHANDLERCOMPONENT_H
